629652ed53c128edb930f717027e46ae5d8dc5be
Linesh <linesh.simpcity@gmail.com>
2020-02-21 22:39:25 +0800

    ♻️refactor: use current date

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,13 +1,13 @@
 export class Calendar {
-  private readonly date: string
+  private readonly date: Date
 
   constructor(date: string) {
-    this.date = date
+    this.date = new Date(date)
   }
 
   public printWeekInfoInText(): string {
     const header: string = 'Su  Mo  Tu  Wn  Th  Fr  Sa'
-    const content: string = '2   3   4   5   6   7   8'
+    const content: string = `${this.date.getDate()}   3   4   5   6   7   8`
     return `${header}\n${content}`
   }
 }
