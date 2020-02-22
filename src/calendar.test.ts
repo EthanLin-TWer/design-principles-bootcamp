174e8d529149b5ae17840537a2375f297973974c
Linesh <linesh.simpcity@gmail.com>
2020-02-22 23:34:13 +0800

    ✨feature: add rests of months

--- src/calendar.test.ts
+++ src/calendar.test.ts
@@ -26,7 +26,7 @@ describe('Calendar', () => {
   })
 
   describe('printing days of next month', () => {
-    it('should print Feb days when date is in the last week of Jan', () => {
+    it('should print February days when date is in the last week of January', () => {
       const result = new Calendar('2020-01-30').printCurrentWeek()
 
       expect(result).toEqual(
@@ -34,7 +34,7 @@ describe('Calendar', () => {
       )
     })
 
-    it('should print Feb 29th when date is in the last week of Feb given current year is a leap year given year is divisible by 4', () => {
+    it('should print February 29th when date is in the last week of February given current year is a leap year given year is divisible by 4', () => {
       const result = new Calendar('2016-02-29').printCurrentWeek()
 
       expect(result).toEqual(
@@ -42,7 +42,7 @@ describe('Calendar', () => {
       )
     })
 
-    it('should print Feb 28th when date is in the last week of Feb given current year is not a leap year given year is not divisible by 4', () => {
+    it('should print February 28th when date is in the last week of February given current year is not a leap year given year is not divisible by 4', () => {
       const result = new Calendar('2019-02-28').printCurrentWeek()
 
       expect(result).toEqual(
@@ -50,20 +50,92 @@ describe('Calendar', () => {
       )
     })
 
-    it('should print Feb 28th when date is in the last week of Feb given current year is not a leap year given year is divisible by 4 but not divisible by 400', () => {
+    it('should print February 28th when date is in the last week of February given current year is not a leap year given year is divisible by 4 but not divisible by 400', () => {
       const result = new Calendar('1900-02-28').printCurrentWeek()
 
       expect(result).toEqual(
         '日\t一\t二\t三\t四\t五\t六\n25  26  27  28  1   2   3'
       )
     })
-  })
 
-  it('should print Apr days when date is in the last week of Mar', () => {
-    const result = new Calendar('2020-03-30').printCurrentWeek()
+    it('should print April days when date is in the last week of March', () => {
+      const result = new Calendar('2020-03-30').printCurrentWeek()
 
-    expect(result).toEqual(
-      '日\t一\t二\t三\t四\t五\t六\n29  30  31  1   2   3   4'
-    )
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n29  30  31  1   2   3   4'
+      )
+    })
+
+    it('should print May days when date is in the last week of April', () => {
+      const result = new Calendar('2020-04-30').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n26  27  28  29  30  1   2'
+      )
+    })
+
+    it('should print June days when date is in the last week of May', () => {
+      const result = new Calendar('2020-05-31').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n31  1   2   3   4   5   6'
+      )
+    })
+
+    it('should print July days when date is in the last week of Jun', () => {
+      const result = new Calendar('2020-06-30').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n28  29  30  1   2   3   4'
+      )
+    })
+
+    it('should print August days when date is in the last week of July', () => {
+      const result = new Calendar('2020-07-30').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n26  27  28  29  30  31  1'
+      )
+    })
+
+    it('should print September days when date is in the last week of August', () => {
+      const result = new Calendar('2020-08-31').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n30  31  1   2   3   4   5'
+      )
+    })
+
+    it('should print October days when date is in the last week of September', () => {
+      const result = new Calendar('2020-09-30').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n27  28  29  30  1   2   3'
+      )
+    })
+
+    it('should print November days when date is in the last week of October', () => {
+      const result = new Calendar('2019-10-31').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n27  28  29  30  31  1   2'
+      )
+    })
+
+    it('should print December days when date is in the last week of November', () => {
+      const result = new Calendar('2020-11-30').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n29  30  1   2   3   4   5'
+      )
+    })
+
+    it('should print January days when date is in the last week of December', () => {
+      const result = new Calendar('2020-12-31').printCurrentWeek()
+
+      expect(result).toEqual(
+        '日\t一\t二\t三\t四\t五\t六\n27  28  29  30  31  1   2'
+      )
+    })
   })
 })
