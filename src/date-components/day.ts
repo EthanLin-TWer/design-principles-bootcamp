cf330e0de0b02c94b3a3140f128c8bc5e5ca3eb2
Linesh <linesh.simpcity@gmail.com>
2020-02-23 14:07:13 +0800

    ♻️refactor: extract addTrailingSpaceForDaysBefore10th()

--- src/date-components/day.ts
+++ src/date-components/day.ts
@@ -4,4 +4,8 @@ export class Day extends DateComponent {
   asDD() {
     return this.asPrintable()
   }
+
+  addTrailingSpaceForDaysBefore10th() {
+    return this.value >= 10 ? this.value.toString() : `${this.value} `
+  }
 }
--- src/printers/console-printer.ts
+++ src/printers/console-printer.ts
@@ -1,9 +1,11 @@
+import { Day } from '../date-components/day'
+
 export class ConsolePrinter {
   // eslint-disable-next-line class-methods-use-this
   public print(headers: string[], data: number[]): string {
     const header = headers.join('\t')
     const content = data
-      .map((date) => (date >= 10 ? date.toString() : `${date} `))
+      .map((date) => new Day(date).addTrailingSpaceForDaysBefore10th())
       .join('  ')
       .trimEnd()
 
