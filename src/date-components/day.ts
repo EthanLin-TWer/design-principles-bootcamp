4ca5980ae80803659efadb5457a13341e8580426
Linesh <linesh.simpcity@gmail.com>
2020-02-23 16:57:00 +0800

    ♻️refactor: final polish

--- src/date-components/day.ts
+++ src/date-components/day.ts
@@ -13,32 +13,40 @@ export class Day extends DateComponent {
 
   public next(fewDays: number): Day {
     const nextDay = this.value + fewDays
-    if (nextDay <= this.month.getTotalDays(this.year)) {
+    if (this.isWithinSameMonth(nextDay)) {
       return new Day(nextDay, this.month, this.year)
     }
 
     const day = nextDay - this.month.getTotalDays(this.year)
-    if (this.month !== Month.DECEMBER) {
+    if (this.isWithinSameYear()) {
       return new Day(day, this.month.next(), this.year)
     }
 
-    return new Day(day, this.month.next(), this.year)
+    return new Day(day, this.month.next(), this.year.next())
   }
 
   public previous(fewDays: number): Day {
     const previousDay = this.value - fewDays
-    if (previousDay > 0) {
+    if (!this.isGoingBackOneMonth(previousDay)) {
       return new Day(previousDay, this.month, this.year)
     }
 
     const totalDaysOfLastMonth = this.month.previous().getTotalDays(this.year)
     const day = previousDay + totalDaysOfLastMonth
 
-    if (this.month !== Month.JANUARY) {
+    if (!this.isGoingBackOneYear()) {
       return new Day(day, this.month.previous(), this.year)
     }
 
     return new Day(day, this.month.previous(), this.year.previous())
+  }
+
+  private isGoingBackOneYear() {
+    return this.month === Month.JANUARY
+  }
+
+  private isGoingBackOneMonth(previousDay: number) {
+    return previousDay <= 0
   }
 
   public addTrailingSpaceForDaysBefore10th() {
@@ -47,6 +55,14 @@ export class Day extends DateComponent {
 
   public asYYYYmmDD() {
     return `${this.year.getValue()}-${this.month.asMM()}-${this.asDD()}`
+  }
+
+  private isWithinSameYear() {
+    return this.month !== Month.DECEMBER
+  }
+
+  private isWithinSameMonth(nextDay: number) {
+    return nextDay <= this.month.getTotalDays(this.year)
   }
 
   private asDD() {
