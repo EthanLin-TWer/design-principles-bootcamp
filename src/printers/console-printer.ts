23c98ad22c3639369a5691e4bb21f7850f6196f7
Linesh <linesh.simpcity@gmail.com>
2020-02-23 14:12:47 +0800

    🔧chore: only warn when methods doesn't uses 'this'

--- src/printers/console-printer.ts
+++ src/printers/console-printer.ts
@@ -1,7 +1,6 @@
 import { Day } from '../date-components/day'
 
 export class ConsolePrinter {
-  // eslint-disable-next-line class-methods-use-this
   public print(headers: string[], days: Day[]): string {
     const header = headers.join('\t')
     const content = days
