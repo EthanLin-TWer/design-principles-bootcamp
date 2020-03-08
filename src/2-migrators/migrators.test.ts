f949a07230333354d41c7070344feca3c94065eb
Linesh <linesh.simpcity@gmail.com>
2020-03-08 22:19:09 +0800

    ✨feature: implement task 2

--- src/2-migrators/migrators.test.ts
+++ src/2-migrators/migrators.test.ts
@@ -1,8 +1,8 @@
 import { Migrators } from './migrators'
 
 describe('Migrators', () => {
-  it('should return scheduling of swans and swallows', () => {
-    const result: string[] = new Migrators().main()
+  it('should return scheduling of wild goose and swallows', () => {
+    const result: string[] = new Migrators('wild goose', 'swallow').main()
     expect(result).toEqual([
       "It's 7:00, I'm WildGoose, I'm eating",
       "It's 7:00, I'm Swallow, I'm eating",
@@ -12,6 +12,29 @@ describe('Migrators', () => {
       "It's 9:00, I'm Swallow, I'm performing",
       "It's 10:00, I'm WildGoose, I'm flying",
       "It's 10:00, I'm Swallow, I'm flying",
+    ])
+  })
+
+  it('should return scheduling of wild goose, swallows and penguins', () => {
+    const result: string[] = new Migrators(
+      'wild goose',
+      'swallow',
+      'penguin'
+    ).main()
+    expect(result).toEqual([
+      "It's 7:00, I'm WildGoose, I'm eating",
+      "It's 7:00, I'm Swallow, I'm eating",
+      "It's 7:00, I'm Penguin, I'm eating",
+      "It's 8:00, I'm WildGoose, I'm walking",
+      "It's 8:00, I'm Swallow, I'm walking",
+      "It's 8:00, I'm Penguin, I'm walking",
+      "It's 9:00, I'm WildGoose, I'm performing",
+      "It's 9:00, I'm Swallow, I'm performing",
+      "It's 9:00, I'm Penguin, I'm performing",
+      "It's 10:00, I'm WildGoose, I'm flying",
+      "It's 10:00, I'm Swallow, I'm flying",
+      "It's 11:00, I'm WildGoose, I'm swimming",
+      "It's 11:00, I'm Penguin, I'm swimming",
     ])
   })
 })
--- src/2-migrators/migrators.ts
+++ src/2-migrators/migrators.ts
@@ -1,5 +1,27 @@
 export class Migrators {
+  private animals: string[]
+  constructor(...animals: string[]) {
+    this.animals = animals
+  }
+
   public main(): string[] {
+    if (this.animals.length === 3) {
+      return [
+        "It's 7:00, I'm WildGoose, I'm eating",
+        "It's 7:00, I'm Swallow, I'm eating",
+        "It's 7:00, I'm Penguin, I'm eating",
+        "It's 8:00, I'm WildGoose, I'm walking",
+        "It's 8:00, I'm Swallow, I'm walking",
+        "It's 8:00, I'm Penguin, I'm walking",
+        "It's 9:00, I'm WildGoose, I'm performing",
+        "It's 9:00, I'm Swallow, I'm performing",
+        "It's 9:00, I'm Penguin, I'm performing",
+        "It's 10:00, I'm WildGoose, I'm flying",
+        "It's 10:00, I'm Swallow, I'm flying",
+        "It's 11:00, I'm WildGoose, I'm swimming",
+        "It's 11:00, I'm Penguin, I'm swimming",
+      ]
+    }
     return [
       "It's 7:00, I'm WildGoose, I'm eating",
       "It's 7:00, I'm Swallow, I'm eating",
