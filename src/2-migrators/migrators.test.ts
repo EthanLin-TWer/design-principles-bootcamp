4815edef1dfa1591f2ae18d408265fb91f34aa36
Linesh <linesh.simpcity@gmail.com>
2020-03-08 22:12:51 +0800

    ✨feature: implement task 1 - literally, seems this is enough atm

--- src/2-migrators/migrators.test.ts
+++ src/2-migrators/migrators.test.ts
@@ -3,6 +3,15 @@ import { Migrators } from './migrators'
 describe('Migrators', () => {
   it('should return scheduling of swans and swallows', () => {
     const result: string[] = new Migrators().main()
-    expect(result).toEqual([])
+    expect(result).toEqual([
+      "It's 7:00, I'm WildGoose, I'm eating",
+      "It's 7:00, I'm Swallow, I'm eating",
+      "It's 8:00, I'm WildGoose, I'm walking",
+      "It's 8:00, I'm Swallow, I'm walking",
+      "It's 9:00, I'm WildGoose, I'm performing",
+      "It's 9:00, I'm Swallow, I'm performing",
+      "It's 10:00, I'm WildGoose, I'm flying",
+      "It's 10:00, I'm Swallow, I'm flying",
+    ])
   })
 })
--- src/2-migrators/migrators.ts
+++ src/2-migrators/migrators.ts
@@ -1,5 +1,14 @@
 export class Migrators {
   public main(): string[] {
-    return []
+    return [
+      "It's 7:00, I'm WildGoose, I'm eating",
+      "It's 7:00, I'm Swallow, I'm eating",
+      "It's 8:00, I'm WildGoose, I'm walking",
+      "It's 8:00, I'm Swallow, I'm walking",
+      "It's 9:00, I'm WildGoose, I'm performing",
+      "It's 9:00, I'm Swallow, I'm performing",
+      "It's 10:00, I'm WildGoose, I'm flying",
+      "It's 10:00, I'm Swallow, I'm flying",
+    ]
   }
 }
