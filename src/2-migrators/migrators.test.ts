446b8ea528ed5ea49ad82c1f7ce2294642a8c902
Linesh <linesh.simpcity@gmail.com>
2020-03-14 22:33:13 +0800

    ♻️refactor: remove one duplicated tests

--- src/2-migrators/migrators.test.ts
+++ src/2-migrators/migrators.test.ts
@@ -4,25 +4,6 @@ import { Swallow } from './animals/Penguin'
 import { Penguin } from './animals/Swallow'
 
 describe('Migrators', () => {
-  it('should return scheduling of wild goose and swallows', () => {
-    const result: string[] = new Migrators(
-      new WildGoose(),
-      new Swallow()
-    ).printScheduling()
-
-    expect(result).toEqual([
-      "It's 7:00, I'm WildGoose, I'm eating",
-      "It's 7:00, I'm Swallow, I'm eating",
-      "It's 8:00, I'm WildGoose, I'm walking",
-      "It's 8:00, I'm Swallow, I'm walking",
-      "It's 9:00, I'm WildGoose, I'm performing",
-      "It's 9:00, I'm Swallow, I'm performing",
-      "It's 10:00, I'm WildGoose, I'm flying",
-      "It's 10:00, I'm Swallow, I'm flying",
-      "It's 11:00, I'm WildGoose, I'm swimming",
-    ])
-  })
-
   it('should return scheduling of wild goose, swallows and penguins', () => {
     const result: string[] = new Migrators(
       new WildGoose(),
