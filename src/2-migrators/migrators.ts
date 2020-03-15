fe7dc9d0ee185386b4232ec9d6cd8d10ac186225
Linesh <linesh.simpcity@gmail.com>
2020-03-15 09:13:33 +0800

    ♻️refactor: replace reduce+concat with flatMap

--- src/2-migrators/migrators.ts
+++ src/2-migrators/migrators.ts
@@ -1,16 +1,14 @@
+import flatMap from 'lodash/flatMap'
+
 import { Bird } from './animals/base'
 import { Schedules } from './schedules/schedules'
-import { Schedule } from './schedules/base'
 
 export class Migrators {
   private schedules: Schedules
 
   constructor(...animals: Bird[]) {
     this.schedules = new Schedules(
-      animals.reduce(
-        (result: Schedule[], next: Bird) => result.concat(next.getSchedules()),
-        []
-      )
+      flatMap(animals, (animal) => animal.getSchedules())
     )
   }
 
