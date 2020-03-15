3a3a4acacdad517e3163c5a8d77cb04e7a39ba73
Linesh <linesh.simpcity@gmail.com>
2020-03-15 09:08:28 +0800

    ♻️refactor: seems Migrators doesn't need to maintain a Bird[] field

--- src/2-migrators/migrators.ts
+++ src/2-migrators/migrators.ts
@@ -3,13 +3,11 @@ import { Schedules } from './schedules/schedules'
 import { Schedule } from './schedules/base'
 
 export class Migrators {
-  private animals: Bird[]
   private schedules: Schedules
 
   constructor(...animals: Bird[]) {
-    this.animals = animals
     this.schedules = new Schedules(
-      this.animals.reduce(
+      animals.reduce(
         (result: Schedule[], next: Bird) => result.concat(next.getSchedules()),
         []
       )
