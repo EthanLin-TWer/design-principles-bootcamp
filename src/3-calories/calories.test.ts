6ad8bfc64909d3e0c31cc8140fe7803ddc64d2c1
Linesh <linesh.simpcity@gmail.com>
2020-03-21 15:05:41 +0800

    ✅test: finish first e2e test case for the calories calculator

--- src/3-calories/calories.test.ts
+++ src/3-calories/calories.test.ts
@@ -1,9 +1,9 @@
-import puppeteer from 'puppeteer'
+import puppeteer, { Browser } from 'puppeteer'
 import StaticServer from 'static-server'
 
 describe('calories', () => {
   let server
-  let browser
+  let browser: Browser
   const port = 1337
   const fileName = 'calories.html'
 
@@ -29,5 +29,58 @@ describe('calories', () => {
     await page.goto(`http://localhost:${port}/${fileName}`, {
       waitUntil: 'load',
     })
+
+    // select sex
+    await page.evaluate(
+      (argument) => document.querySelector(argument).click(),
+      '#sex-male'
+    )
+    // enter weight
+    const weightInputId = 'input[name="weight"]'
+    await page.evaluate(
+      (argument) => (document.querySelector(argument).value = ''),
+      weightInputId
+    )
+    await page.type(weightInputId, '120')
+
+    // enter height - feet
+    const feetInputId = 'input[name="height_of_feet"]'
+    await page.evaluate(
+      (argument) => (document.querySelector(argument).value = ''),
+      feetInputId
+    )
+    await page.type(feetInputId, '4')
+
+    // enter height - feet
+    const inchInputId = 'input[name="height_of_inch"]'
+    await page.evaluate(
+      (argument) => (document.querySelector(argument).value = ''),
+      inchInputId
+    )
+    await page.type(inchInputId, '2')
+
+    // enter age
+    const ageInputId = 'input[name="age"]'
+    await page.evaluate(
+      (argument) => (document.querySelector(argument).value = ''),
+      ageInputId
+    )
+    await page.type(ageInputId, '18')
+
+    // click calculate
+    await page.evaluate(
+      (argument) => document.querySelector(argument).click(),
+      'button#calculate'
+    )
+
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    // expect output result
+    const result = await page.evaluate(
+      (argument) => document.querySelector(argument).innerHTML,
+      'label[name="recommanded_calories"]'
+    )
+
+    expect(result).toEqual('1344.6')
   })
 })
