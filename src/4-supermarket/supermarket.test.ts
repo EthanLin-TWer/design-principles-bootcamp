c4a4b8bd728741b0ef7d9e5ffdabfd5802f46986
Linesh <linesh.simpcity@gmail.com>
2020-04-06 17:01:36 +0800

    ✨feature: task 2: price reduction after reaching total amount

--- src/4-supermarket/supermarket.test.ts
+++ src/4-supermarket/supermarket.test.ts
@@ -9,9 +9,23 @@ describe('SuperMarket', () => {
     { name: 'light', category: 'electronics', price: 100, quantity: 1 },
   ]
 
-  describe('task 1: 80% discount', () => {
+  describe.skip('task 1: 80% discount', () => {
     it('should calculate total price and apply a 80% discount', () => {
       const result = new Calculator().calculate(products)
+
+      expect(result).toEqual(180)
+    })
+  })
+
+  describe('task 2: amount reduction', () => {
+    it('should reduce ￥5 when total price is more than ￥50', () => {
+      const result = new Calculator().calculate([
+        { name: 'wine', category: 'drink', price: 15, quantity: 1 },
+        { name: 'cola', category: 'drink', price: 5, quantity: 2 },
+        { name: 'pork', category: 'meat', price: 25, quantity: 2 },
+        { name: 'chicken', category: 'meat', price: 10, quantity: 5 },
+        { name: 'light', category: 'electronics', price: 100, quantity: 1 },
+      ])
 
       expect(result).toEqual(180)
     })
