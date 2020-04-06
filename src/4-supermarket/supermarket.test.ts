b38aff65602329dbf77fd11cac897de9dca67500
Linesh <linesh.simpcity@gmail.com>
2020-04-06 17:04:52 +0800

    ✨feature: should not get any discount when total price is less than ￥50

--- src/4-supermarket/supermarket.test.ts
+++ src/4-supermarket/supermarket.test.ts
@@ -9,7 +9,7 @@ describe('SuperMarket', () => {
     { name: 'light', category: 'electronics', price: 100, quantity: 1 },
   ]
 
-  describe.skip('task 1: 80% discount', () => {
+  describe.skip('task 1: 20% off', () => {
     it('should calculate total price and apply a 80% discount', () => {
       const result = new Calculator().calculate(products)
 
@@ -17,17 +17,23 @@ describe('SuperMarket', () => {
     })
   })
 
-  describe('task 2: amount reduction', () => {
-    it('should reduce ￥5 when total price is more than ￥50', () => {
+  describe('task 2: cash back', () => {
+    it('should not get any cash back when total price is less than ￥50', () => {
       const result = new Calculator().calculate([
         { name: 'wine', category: 'drink', price: 15, quantity: 1 },
         { name: 'cola', category: 'drink', price: 5, quantity: 2 },
-        { name: 'pork', category: 'meat', price: 25, quantity: 2 },
-        { name: 'chicken', category: 'meat', price: 10, quantity: 5 },
-        { name: 'light', category: 'electronics', price: 100, quantity: 1 },
       ])
 
-      expect(result).toEqual(180)
+      expect(result).toEqual(25)
+    })
+
+    it('should get ￥5 cash back when total price is more than ￥50', () => {
+      const result = new Calculator().calculate([
+        { name: 'wine', category: 'drink', price: 15, quantity: 1 },
+        { name: 'cola', category: 'drink', price: 5, quantity: 2 },
+      ])
+
+      expect(result).toEqual(25)
     })
   })
 })
--- src/4-supermarket/supermarket.ts
+++ src/4-supermarket/supermarket.ts
@@ -1,11 +1,10 @@
 export class Calculator {
   calculate(products: any[]) {
-    const DISCOUNT = 0.8
     const totalPrice = products.reduce(
       (result, product) => result + product.price * product.quantity,
       0
     )
 
-    return totalPrice * DISCOUNT
+    return totalPrice
   }
 }
