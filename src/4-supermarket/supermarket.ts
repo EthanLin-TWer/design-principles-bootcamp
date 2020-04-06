8c93cd7b53ad1874e99cc20d8ab929030ce7e3f4
Linesh <linesh.simpcity@gmail.com>
2020-04-06 17:14:23 +0800

    ♻️refactor: extract discount()

--- src/4-supermarket/supermarket.ts
+++ src/4-supermarket/supermarket.ts
@@ -2,11 +2,12 @@ export class Calculator {
   calculate(products: any[]) {
     return products.reduce(
       (result, product) =>
-        result +
-        product.price *
-          product.quantity *
-          (product.category === 'drink' ? 0.8 : 1),
+        result + product.price * product.quantity * this.discount(product),
       0
     )
+  }
+
+  private discount({ category }) {
+    return category === 'drink' ? 0.8 : 1
   }
 }
